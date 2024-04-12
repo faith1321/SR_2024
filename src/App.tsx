70866e31921c2b9a166825e895da6c53c4af6169
Faith <faith_new@hotmail.com>
2024-04-12 22:18:36 +0100

    ✨ Added PrimeReact and PrimeFlex

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {PrimeReactProvider} from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "./App.css";
+import HomePage from "./features/HomePage";
+import "/node_modules/primeflex/primeflex.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<PrimeReactProvider>
+			<div className="App">
+				<header className="App-header">
+					{/* <img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a> */}
+					<HomePage />
+				</header>
+			</div>
+		</PrimeReactProvider>
+	);
 }
 
 export default App;
