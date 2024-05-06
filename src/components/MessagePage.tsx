53178f34aadc3f196bd5b577939660479f516ea0
Faith <faith_new@hotmail.com>
2024-05-06 21:45:09 +0100

    ✨ Added image to MessagePage

--- src/components/MessagePage.tsx
+++ src/components/MessagePage.tsx
@@ -1,46 +1,117 @@
 import {collection, getDocs} from "firebase/firestore";
-import {useEffect, useState} from "react";
+import {getDownloadURL, getStorage, ref} from "firebase/storage";
+import {
+	MotionValue,
+	motion,
+	useScroll,
+	useSpring,
+	useTransform,
+} from "framer-motion";
+import {useEffect, useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {db} from "../firebase/firebase-config";
 import {userConverter} from "../firebase/firebaseUtils";
 import "./HomePage.css";
+import "./MessagePage.css";
 import {loadState} from "./localStorageUtils";
 
-type states = "" | "correct" | "wrong" | null;
-
 const Message = () => {
+	const [data, setData] = useState<string[]>();
 	const [name, setName] = useState("");
 	const [msg, setMsg] = useState("");
+	const [url, setURL] = useState("");
 	const navigate = useNavigate();
 
 	const usersCollectionRef = collection(db, "users").withConverter(
 		userConverter
 	);
 
-	setName(loadState("name"));
+	useEffect(() => {
+		setName(loadState("name"));
+		loadData();
+	}, []);
 
-	const loadMsg = async () => {
+	const loadData = async () => {
 		const usersData = await getDocs(usersCollectionRef);
 
 		usersData.forEach((doc) => {
 			// doc.data() is never undefined for query doc snapshots
 			console.log(doc.id, " => ", doc.data());
 			if (doc.data().name === name) {
+				setData(doc.data().images);
 				setMsg(doc.data().msg);
 			}
 		});
 	};
 
-	useEffect(() => {
-		loadMsg();
+	function useParallax(value: MotionValue<number>, distance: number) {
+		return useTransform(value, [0, 1], [-distance, distance]);
+	}
+
+	function Image({name}: {name: string}) {
+		const ref = useRef(null);
+		const {scrollYProgress} = useScroll({target: ref});
+		const y = useParallax(scrollYProgress, 300);
+
+		return (
+			<section>
+				<div ref={ref}>
+					<img src={url} alt="Cloud Storage" />
+				</div>
+				<motion.h2 style={{y}}>{name}</motion.h2>
+			</section>
+		);
+	}
+
+	const Paragraph = () => {
+		const ref = useRef(null);
+		const {scrollYProgress} = useScroll({
+			target: ref,
+			offset: ["end", "start"],
+		});
+		const scaleX = useSpring(scrollYProgress);
+
+		return (
+			<p className="header-p">
+				<motion.div style={{scaleX}}>{msg}</motion.div>
+			</p>
+		);
+	};
+
+	const reference = useRef(null);
+	const {scrollYProgress} = useScroll({
+		target: reference,
+	});
+	const scaleX = useSpring(scrollYProgress, {
+		stiffness: 100,
+		damping: 30,
+		restDelta: 0.001,
 	});
 
+	const getImageURL = async () => {
+		const storage = getStorage();
+		const storageRef = ref(storage, "SR_2024/images/Rui Zhe/093318980015.jpg");
+
+		let url = "";
+		url = await getDownloadURL(storageRef);
+		return url;
+	};
+
+	getImageURL().then((result) => {
+		setURL(result);
+		return;
+	});
+
+	console.log("result: " + url);
+
 	return (
-		<div className="flex flex-column gap-5 homepage">
-			<header>
-				<p className="header-p">{msg}</p>
-			</header>
-		</div>
+		<header>
+			<Paragraph />
+			<motion.div className="progress" style={{scaleX}} />
+			{["SR_2024/images/Rui_Zhe/093318980015.jpg"].map((image) => (
+				<Image name={image} key={image} />
+			))}
+		</header>
 	);
 };
 
--- src/firebase/firebaseUtils.tsx
+++ src/firebase/firebaseUtils.tsx
@@ -4,11 +4,13 @@ class User {
 	name: string;
 	pw: string;
 	msg: string;
+	images: string[];
 
-	constructor(name: string, pw: string, msg: string) {
+	constructor(name: string, pw: string, msg: string, images: string[]) {
 		this.name = name;
 		this.pw = pw;
 		this.msg = msg;
+		this.images = images;
 	}
 	toString() {
 		return this.name + ", " + this.pw + ", " + this.msg;
@@ -16,11 +18,17 @@ class User {
 }
 // Firestore data converter
 export const userConverter = {
-	toFirestore: (user: {name: string; pw: string; msg: string}) => {
+	toFirestore: (user: {
+		name: string;
+		pw: string;
+		msg: string;
+		images: string[];
+	}) => {
 		return {
 			name: user.name,
-			state: user.pw,
-			country: user.msg,
+			password: user.pw,
+			message: user.msg,
+			images: user.images,
 		};
 	},
 	fromFirestore: (
@@ -28,6 +36,6 @@ export const userConverter = {
 		options: SnapshotOptions
 	) => {
 		const data = snapshot.data(options);
-		return new User(data.name, data.pw, data.msg);
+		return new User(data.name, data.pw, data.msg, data.images);
 	},
 };
